93ca07045c9665176d357ca629cf545ff7c0735e
Evelyn Zouras <ezouras@hotmail.com>
2018-06-05 15:53:08 -0500

    required path

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express=require('express');
+const path = require('path');
 const app= express();
 
 
 const port = process.env.PORT || 3000;
+const indexFile=path.join(__dirname + '/Public/index.html');
+console.log("directory to index is: ",indexFile);
 
+app.get('/', function(request, response) {
+response.sendFile(indexFile);
+});
 
 app.use(express.static(__dirname + '/Public'));
-app.get('/', function(request, response) {
-response.sendFile(path.join(__dirname + '/Public/index.html'));
-});
 
 
 app.listen(port,()=>{

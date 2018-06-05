fb96d0b2e9936cba96b76e5b8432ea89ec4e4c05
Evelyn Zouras <ezouras@hotmail.com>
2018-06-05 15:47:28 -0500

    send index for /

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/Public'));
 app.get('/', function(request, response) {
-response.send('Hello World!');
+response.sendFile(path.join(__dirname + '/Public/index.html'));
 });
+
 
 app.listen(port,()=>{
   console.log("server up!");

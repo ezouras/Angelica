c3666e9e4b898d92b6c7461b49c426f0ba29ffc2
Evelyn Zouras <ezouras@hotmail.com>
2018-07-10 14:37:53 -0500

    Release 1

---
+++ Public/JS/Angelica.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+//glitch, then fade out to static.
+
+
+  //* for 5 seconds, play the static animation.
+
+
+setTimeout(function()
+  {
+
+    $( "#intro" ).addClass( "hide" );
+    $("#mainBlock").removeClass("hide");
+
+  }, 2000);
+
+
+
+
+
+
+
+});
+
+/* show main page for one second glitch , fade,
+and then show static */

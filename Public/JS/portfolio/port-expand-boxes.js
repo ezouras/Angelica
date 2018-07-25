40a4670340ea6daf9c37884ff631e94bf4ea018b
Evelyn Zouras <ezouras@hotmail.com>
2018-07-24 21:49:38 -0500

    finished photo portfolio

--- Public/JS/portfolio/port-expand-boxes.js
+++ Public/JS/portfolio/port-expand-boxes.js
@@ -1,46 +1,108 @@
+var bColor;
+//makeup box IDs
+var mkupMainBoxPostText="-mkup";
+var mkupOverlayPostText="-layer-mkup";
+var mkupAbsoluteBoxPreText="innerAbMU-";
+
+//photography box IDs
+var photoMainBoxPostText="-photo";
+var photoOverlayPostText="-layer-photo";
+var photoAbsoluteBoxPreText="innerAbPH-";
+var currentMainBox;
+var currentAbsoluteBox;
+var currentOverlay;
+var lastMainBox;
+var lastAbsoluteBox;
+var lastOverlay;
+
 $(document).ready(function(){
 
-  //get #portfolio element.
-  $("#portfolio").hover(()=>{
-    $("#port-banner").css("animation-name","shrinkWidth");
+  //animate portfolio banner//
+  $(".portfolio").hover(()=>{
+    $(".port-banner").css("animation-name","shrinkWidth");
     $(".bAnimate").css("animation-name","growHeight");
   },()=>{
-      $("#port-banner").css("animation-name","growWidth");
+      $(".port-banner").css("animation-name","growWidth");
     $(".bAnimate").css("animation-name","shrinkHeight")
   });
 
-/////*****  sample rectangle animation *****/////
-
-  //when mousing over a rectangle sample, expand it
-
-  $( ".bInner" ).mouseenter(function(e) {
-    var boxSelection=getBoxSelection($(this).attr('class').split(" "));
-    $(boxSelection).css( "border", "3px solid red" );
-    //$(boxSelection).css("animation","grow .5s ease");
-    $(boxSelection).animate({
-    width: "75%",
-    height:"120%"
-    },
-    300,
-    $.bez([0.685, 0.595, 0.020, 0.720]));
-
+  $( ".bInner" ).mouseenter(function(e){
+    var boxID=getBoxId(this);
+    setBoxValues(boxID,"current");
+    setPreBackgroundColor(currentOverlay);
+    $("#" +currentMainBox).css( "border", "1px solid red" );
+    animateGrow("#"+currentMainBox);
+    $("#"+currentOverlay).css( "background-color", "rgba(88, 0, 0, .2)");
   });
 
-  //when leaving a sample rectangle, animate the shape to it's default size.
- $( ".bInner" ).mouseleave(function(e) {
-   var boxSelection=getBoxSelection($(this).attr('class').split(" "));
-       $(boxSelection).css( "border", "none");
-       $(boxSelection).animate({
-       width: "25%",
-       height:"100%"
-     }, 300);
+
+ $( ".bInner" ).mouseleave(function(e){
+  var boxID=getBoxId(this);
+  setBoxValues(boxID,"last");
+  $("#"+lastMainBox).css( "border", "none");
+  animateShrink("#"+lastMainBox);
+  setPostBackgroundColor("#"+lastOverlay);
  });
 
 });//end jquery
 
+function getBoxId(boxElement){
+  return $(boxElement).attr('id');
+}
 
-function getBoxSelection(classList){
-  var position=classList.filter(value=>value.includes("p"));
-  position=position[0].replace(/\D/g,'');
-  return ".b"+position;
+function setBoxValues(innerMainBoxID,timing){
+  //innerAbMU-1
+  var type=innerMainBoxID.substring(7,9);
+  var id=innerMainBoxID[innerMainBoxID.length-1];
+  if(timing==="current")
+  {
+    if(type==="MU")
+    {
+      currentMainBox="b"+id+mkupMainBoxPostText;
+      currentOverlay="b"+id+mkupOverlayPostText;
+    }
+    else{
+    currentMainBox="b"+id+photoMainBoxPostText;
+    currentOverlay="b"+id+photoOverlayPostText;
+  }
+  }else{
+    if(type==="MU")
+    {
+      lastMainBox="b"+id+mkupMainBoxPostText;
+      lastOverlay="b"+id+mkupOverlayPostText;
+    }
+    else{
+      lastMainBox="b"+id+photoMainBoxPostText;
+      lastOverlay="b"+id+photoOverlayPostText;
+    }
+}
+  return;
+}
+
+
+function setPreBackgroundColor(selection){
+  bColor=$("#"+selection).css("background-color");
+}
+
+function setPostBackgroundColor(element){
+  $(element).css( "background-color",bColor);
+}
+
+
+function animateGrow(element){
+  $(element).animate({
+  width: "100%",
+  height:"120%"
+  },
+  300,
+  $.bez([0.685, 0.595, 0.020, 0.720]));
+}
+
+function animateShrink(element){
+  $(element).css( "border", "none");
+       $("#"+lastMainBox).animate({
+       width: "25%",
+       height:"100%"
+     }, 300);
+
 }
